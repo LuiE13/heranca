b8edc72edd48f5f6631d9319c2b27cd6d6cf80fb
LuiE13 <159495123+LuiE13@users.noreply.github.com>
2024-04-23 09:56:21 -0300

    Add files via upload

---
+++ people/src/Pessoa.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Pessoa {
+    private String nome;
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return (nome);
+    }
+}
---
+++ people/src/PessoaFisica.java
@@ -0,0 +1,10 @@
+public class PessoaFisica extends Pessoa{
+    private String rg;
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getRg() {
+        return (rg);
+    }
+}
